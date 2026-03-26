4dfa5550507d172fa5f1c69a59fd0e70ec621741
Durgesh Vishwakarma <vishwakarmadurgesh21@gmail.com>
2026-03-26 19:45:42 +0530

    yess

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,15 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
   trailingSlash: true,
   images: {
     unoptimized: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
   },
 };
 
